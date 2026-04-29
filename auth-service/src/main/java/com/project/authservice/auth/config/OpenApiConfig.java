2b575f7f12c3b1f1722a39b8c3e0fe65a3eacf6e
Aslan <tynyshtykaslan06@gmail.com>
2026-04-29 20:35:17 +0500

    feat(auth): OpenAPI/Swagger docs

---
+++ auth-service/src/main/java/com/project/authservice/auth/config/OpenApiConfig.java
@@ -0,0 +1,30 @@
+package com.project.authservice.auth.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.*;
+import io.swagger.v3.oas.models.security.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI authServiceOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Auth Service API")
+                        .description("JWT authentication, refresh tokens, account management")
+                        .version("1.0.0")
+                        .contact(new Contact()
+                                .name("Multi-City Travel Team")))
+                .addSecurityItem(new SecurityRequirement().addList("BearerAuth"))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("BearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .description("Paste your access token here")));
+    }
+}
